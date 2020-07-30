c70e0099bcc889c03ba8dfb6c16ae28c880871e7
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 14:11:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 def reverse_each_word(string)
   splitstring = string.split
   binding.pry
-  string.each do
-    string.reverse
+  splitstring.each do |value|
+    value.reverse
   end
 end
