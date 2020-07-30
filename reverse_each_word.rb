580188b2494e55fb484e7864fa501be960a96d8f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 14:54:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,9 +6,10 @@ def reverse_each_word(string)
   string = string.split
   string.collect do |value|
     reverse_string.push(value.reverse)
-    binding.pry
+    
   end
   reverse_string = reverse_string.join(" ")
+  binding.pry
   reverse_string
 end
 
